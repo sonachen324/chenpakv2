475fef6a7d5625343c85077b61c19f8a7abba8f2
sonachen324 <sonachen324@gmail.com>
2024-10-07 15:31:57 +0530

    Add files via upload

---
+++ 必看.cpp
@@ -0,0 +1,4 @@
+Reminder!
+Do not try to crack the tool file
+This tool has added format verification
+If the tool detects that you have cracked, all your files will be deleted! Already reminded, crackers are at their own risk!
